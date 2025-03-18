bbf6bb1ac17fbcbcc31833236da276baa34101e9
Akhil Dakinedi <akhil.dakinedi@doordash.com>
2025-03-17 22:08:52 -0700

    Initial Commit

---
+++ DasherHomescreen/DasherHomescreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DasherHomescreenApp.swift
+//  DasherHomescreen
+//
+//  Created by Akhil Dakinedi on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DasherHomescreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
